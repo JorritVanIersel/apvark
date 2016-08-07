58531d9c41f345a52b72b99725cb73c2bdbc006d
supervarken <jorritspongebob@hotmail.com>
2016-08-07 20:29:36 +0200

    full support for touchscreens

--- public/client.js
+++ public/client.js
@@ -8,35 +8,32 @@ document.addEventListener("DOMContentLoaded", function() {
       colour: "blue"
 
    };
+   var touch = is_touch_device();
 
    var canvas  = document.getElementById('drawing');
 
    var ctx = canvas.getContext('2d');
-   var width   = window.innerWidth;
-   var height  = window.innerHeight;
+   var width = document.body.clientWidth;
+
+var height = document.body.clientHeight;
 
    canvas.width = width;
    canvas.height = height;
 
    canvas.onmousedown = function(e){ mouse.click = true; };
-    canvas.onmouseup = function(e){ mouse.click = false; };
-    canvas.ontouchstart = function(e){ mouse.click = true; };
-    canvas.ontouchend = function(e){ mouse.click = false; };
+   canvas.onmouseup = function(e){ mouse.click = false; };
+   canvas.ontouchstart = function(e){ mouse.click = true;mouse.pos_prev.x = e.touches[0].clientX / width;mouse.pos_prev.y = e.touches[0].clientY / height;};
+   canvas.ontouchend = function(e){ mouse.click = false; };
 
    canvas.onmousemove = function(e) {
-
       mouse.pos.x = e.clientX / width;
       mouse.pos.y = e.clientY / height;
       mouse.pos.w = document.getElementById("rag").value;
       mouse.move = true;
    };
     canvas.ontouchmove = function(e) {
-      var touch = e.touches[0];
-      var x = touch.pageX - canvas.offsetLeft;
-      var y = touch.pageY - canvas.offsetTop;
-      mouse.pos.x = x / width;
-      mouse.pos.y = y / height;
-      console.log(mouse.pos.x);
+      mouse.pos.x = e.touches[0].clientX / width;
+      mouse.pos.y = e.touches[0].clientY / height;
       mouse.pos.w = document.getElementById("rag").value;
       mouse.move = true;
    };
@@ -64,9 +61,15 @@ document.addEventListener("DOMContentLoaded", function() {
          mouse.colour = document.getElementById("col").value;
          socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev, mouse.colour]});
          mouse.move = false;
-      }
-      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-      setTimeout(mainLoop, 20);
+         if (touch){
+             mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+         }
+     }
+
+        mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+
+      setTimeout(mainLoop, 10);
+
    }
 
       mainLoop();
@@ -79,4 +82,11 @@ if(confirm('Are you sure you want to delete everything drawn?')){
     }
 }
 
-
+function is_touch_device() {
+  try {
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
--- server.js
+++ server.js
@@ -3,45 +3,36 @@ var express = require('express'),
     http = require('http'),
     socketIo = require('socket.io');
 
-
 var server =  http.createServer(app);
 var io = socketIo.listen(server);
-
 var port = process.env.PORT || 88;
-//server.listen(port);
 
 app.use(express.static(__dirname + '/public'));
-
 app.get('/reset', function(req, res) {
     res.send('Resetting');
     line_history = [];
     io.emit('clear');
 });
 
-console.log("Server running on 127.0.0.1:8080");
-
 var line_history = [];
 
 io.on('connection', function (socket) {
-
 
    for (var i = 0; i < line_history.length; i++) {
       socket.emit('draw_line', { line: line_history[i] } );
    }
 
-
    socket.on('draw_line', function (data) {
 
       line_history.push(data.line);
 
-
-
       io.emit('draw_line', { line: data.line });
+
    });
 
-    socket.on('clear_all', function(){
-         line_history = [];
-        io.emit('clear');
+   socket.on('clear_all', function(){
+      line_history = [];
+       io.emit('clear');
     })
 });
 
